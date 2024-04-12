33b9dbb09680aaf5252eb123de49bb5a40d98134
Dennis <dennis.lincango@epn.edu.ec>
2024-04-12 09:47:46 -0500

    fix: use the principle of immutable data to use widgets

--- lib/screens/second_screen.dart
+++ lib/screens/second_screen.dart
@@ -7,40 +7,41 @@ import '../widgets/listview.dart';
 class SecondScreen extends StatelessWidget {
   const SecondScreen({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:  const Text('Second Screen'),
+        title:   const Text('Second Screen'),
       ),
       body: ListView(
-        padding:   const EdgeInsets.all(10),
+        padding:    const EdgeInsets.all(10),
         children:  [
           ListTileWidget(
               title: "texto1",
-              subtitle: "subtexto1"),
+              subtitle: "subtexto1",
+            listTileColor: Colors.blue.shade50,
+            leadingIcon: Icons.access_time_filled,
+            trailingIcon: Icons.access_alarms,
+            iconColor: Colors.amber,
+          ),
           ListTileWidget(
-            title: "texto1",
+            title: "texto2",
             subtitle: "subtexto2",
-            leadingIcon: Icons.laptop,
+            listTileColor: Colors.amber.shade50,
+            leadingIcon: Icons.account_box,
+            trailingIcon: Icons.ac_unit,
+            iconColor: Colors.green,
           ),
           ListTileWidget(
               title: "texto3",
               subtitle: "subtexto3",
-              listTileColor: Colors.yellow,
-              trailingIcon: Icons.laptop,
+              listTileColor: Colors.yellow.shade50,
+              leadingIcon: Icons.account_box,
+              trailingIcon: Icons.ac_unit,
               iconColor: Colors.green
           )
         ],
       ),
-
-      // ElevatedButton(
-      //   child: Text('Go To First Screen'),
-      //   onPressed: () {
-      //     Navigator.push(context, MaterialPageRoute(builder: (context) => FirstScreen()));
-      //   }
-      // )
     );
   }
 }
--- lib/widgets/listview.dart
+++ lib/widgets/listview.dart
@@ -2,35 +2,35 @@ import 'package:flutter/material.dart';
 
 class ListTileWidget extends StatelessWidget {
 
-  String title, subtitle;
-  IconData leadingIcon, trailingIcon;
-  Color? listTileColor, iconColor;
+  final String title, subtitle;
+  final IconData leadingIcon, trailingIcon;
+  final Color? listTileColor, iconColor;
 
-  ListTileWidget({super.key,
+  const ListTileWidget({super.key,
     required this.title,
     required this.subtitle,
-      this.leadingIcon = Icons.label,
-      this.trailingIcon = Icons.add_shopping_cart,
-      this.listTileColor,
-      this.iconColor
+    required  this.leadingIcon,
+    required  this.trailingIcon,
+    required  this.listTileColor,
+    required  this.iconColor
   });
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding:   const EdgeInsets.all(5),
+        padding:     const EdgeInsets.all(5),
         child: ListTile(
           tileColor: listTileColor,
           shape: ContinuousRectangleBorder(
             borderRadius: BorderRadius.circular(10),
-            side:  const BorderSide(
+            side:    const BorderSide(
                 width: 1,
                 color: Colors.blue),
           ),
           title: Text(
               title,
-              style:  const TextStyle(fontSize: 20)),
-          subtitle: Text(subtitle, style:  const TextStyle(fontSize: 15)),
+              style:    const TextStyle(fontSize: 20)),
+          subtitle: Text(subtitle, style:    const TextStyle(fontSize: 15)),
           leading: IconButton(
             icon: Icon(leadingIcon), onPressed: (){} ,
             color: iconColor),
