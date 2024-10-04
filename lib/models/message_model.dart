e2c2c247e3bc2d5798972dec23844dda49544352
Dennis Lincango <76795089+dennis-lincango@users.noreply.github.com>
2024-10-04 18:08:58 -0500

    chore: deploy updated Flutter web application

--- lib/models/message_model.dart
+++
@@ -1,9 +0,0 @@
-enum FromWho { me, other }
-
-class MessageModel {
-  final String text;
-  final String? imageUrl;
-  final FromWho fromWho;
-
-  MessageModel({required this.text, this.imageUrl, required this.fromWho});
-}
